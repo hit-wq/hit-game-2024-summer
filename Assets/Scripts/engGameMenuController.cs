7fe4941b5d1bb5b09702ff1541ab9d61687fc428
wzl-tree <usebxc4A>
2024-08-03 15:29:17 +0800

    init

---
+++ Assets/Scripts/engGameMenuController.cs
@@ -0,0 +1,30 @@
+using UnityEngine.UIElements;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class engGameMenuController : MonoBehaviour
+{  
+    void OnEnable()
+    {
+
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        
+        Button replayButton = root.Q<Button>("replayButton");
+        replayButton.clicked += () => this.OnReplayClicked();    
+
+        Button backButton = root.Q<Button>("backButton");
+        backButton.clicked += () => this.OnBackClicked();   
+    }
+
+    // When the back button is pressed, load the start menu scene.
+    public void OnBackClicked()
+    {
+        SceneManager.LoadScene("startGameUI");
+    }
+
+    // When the replay button is pressed, load the game scene.
+    public void OnReplayClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Scripts/startGameMenuController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class startGameMenuController : MonoBehaviour
+{
+    void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button startgamebutton = root.Q<Button>("startgamebutton");
+        startgamebutton.clicked += () => this.OnStartClicked();
+
+    }
+    
+    // When the start button is pressed, load the Game scene.
+    public void OnStartClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
